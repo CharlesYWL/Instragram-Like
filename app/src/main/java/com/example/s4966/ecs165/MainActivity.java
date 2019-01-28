4f88377c6c948aeef0307351b3d5020c47a9e3bd
CharlesYWL <34050457+CharlesYWL@users.noreply.github.com>
2019-01-28 01:38:34 -0800

    something goes wrong here in this branch

--- app/src/main/java/com/example/s4966/ecs165/MainActivity.java
+++ app/src/main/java/com/example/s4966/ecs165/MainActivity.java
@@ -4,12 +4,27 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import android.util.Log;
+import com.google.firebase.analytics.FirebaseAnalytics;
+import com.google.firebase.database.*;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.DataSnapshot;
+
+import org.jetbrains.annotations.NotNull;
+
+
 public class MainActivity extends AppCompatActivity {
 
     // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
     }
+    private static final String TAG = "MainActivity";
+    //firebase innit
+    private DatabaseReference mDatabase;
+    private FirebaseAnalytics mFirebaseAnalytics;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +34,57 @@ public class MainActivity extends AppCompatActivity {
         // Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
         tv.setText(stringFromJNI());
+        TextView myName = (TextView) findViewById(R.id.MyName);
+        myName.setText("Alice");
+
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+
+        //init databse ref
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        // set Listener
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+
+//        writeNewUser("Weili","ddf567");
+//        updateName(Weili,"xiaoyin");
+
+
+
+
     }
+
+    //for write new user
+    private void writeNewUser(String name, String Userid) {
+        User user = new User(name, Userid);
+
+        mDatabase.child("users").child(Userid).setValue(user);
+    }
+
+    @org.jetbrains.annotations.NotNull
+    @org.jetbrains.annotations.Contract(pure = true)
+    private String getName(String UserId){
+        return "";
+        //mDatabase.orderByChild("users").equalTo(UserId)
+    }
+    private void updateName(@NotNull User user, String name){
+        user.username = name;
+        mDatabase.child("users").child(user.ID).child("name").setValue(name);
+    }
+
 
     /**
      * A native method that is implemented by the 'native-lib' native library,
---
+++ app/src/main/java/com/example/s4966/ecs165/User.java
@@ -0,0 +1,16 @@
+package com.example.s4966.ecs165;
+
+public class User {
+    public String username;
+    public String ID;
+    public String Gender;
+    public String bio;
+
+    public User(){}
+
+    public User(String username, String ID){
+        this.username = username;
+        this.ID = ID;
+    }
+
+}
