cf904ae4450118fd1339f7e74791d9a6bdcad971
CharlesYWL <xlyin@ucdavis.edu>
2019-02-11 00:40:03 -0800

    BUG:onClick on GenderChange&BioChange

---
+++ app/src/main/java/com/example/s4966/ecs165/BioChange.java
@@ -0,0 +1,39 @@
+package com.example.s4966.ecs165;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.View;
+
+public class BioChange extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+    //need to use for every class
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.toolbar_menu,menu);
+        return true;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bio_change);
+        //toolbar apply to all
+        mToolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setBackWork(mToolbar);
+    }
+
+
+    public void setBackWork(Toolbar tb){
+        tb.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/example/s4966/ecs165/PictureChange.java
@@ -0,0 +1,37 @@
+package com.example.s4966.ecs165;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.View;
+
+public class PictureChange extends AppCompatActivity {
+
+    private Toolbar mToolbar;
+    //need to use for every class
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.toolbar_menu,menu);
+        return true;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_picture_change);
+        //toolbar apply to all
+        mToolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        setBackWork(mToolbar);
+    }
+    public void setBackWork(Toolbar tb){
+        tb.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+}
