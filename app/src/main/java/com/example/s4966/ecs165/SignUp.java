03fdc99c11deeaa303c95a8e5d29e2d81035883c
CharlesYWL <xlyin@ucdavis.edu>
2019-02-12 10:40:06 -0800

    finished Email Register

--- app/src/main/java/com/example/s4966/ecs165/SignUp.java
+++ app/src/main/java/com/example/s4966/ecs165/SignUp.java
@@ -1,15 +1,45 @@
 package com.example.s4966.ecs165;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.os.Build;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.AppCompatButton;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 public class SignUp extends LoginActivity{
 
     private ProgressBar progressBar;
+    private TextView emailT;
+    private TextView passwordT;
+    private TextView nameT;
+    private AppCompatButton appCompatButton;
+    public static String TAG= "SignUp ";
+    private FirebaseAuth mAuth;
+    private DatabaseReference databaseReference;
+    private StorageReference storageReference;
 
     private Toolbar mToolbar;
     //need to use for every class
@@ -32,10 +62,24 @@ public class SignUp extends LoginActivity{
         progressBar = findViewById(R.id.progressBar);
         progressBar.setVisibility(View.GONE);
 
+        //init all
+        mAuth = FirebaseAuth.getInstance();
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+        storageReference = FirebaseStorage.getInstance().getReference();
+        emailT= findViewById(R.id.input_email);
+        passwordT = findViewById(R.id.input_password);
+        nameT = findViewById(R.id.input_username);
+        appCompatButton = findViewById(R.id.btn_register);
+
+        appCompatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                attemptRegister();
+            }
+        });
+
 
     }
-
-
 
 
     //make back Navi on tool bar works
@@ -47,4 +91,94 @@ public class SignUp extends LoginActivity{
             }
         });
     }
+
+    public void attemptRegister(){
+
+        // Reset errors.
+        emailT.setError(null);
+        passwordT.setError(null);
+
+        // Store values at the time of the login attempt.
+        String email = emailT.getText().toString();
+        String password = passwordT.getText().toString();
+
+        boolean cancel = false;
+        View focusView = null;
+
+        // Check for a valid password, if the user entered one.
+        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
+            passwordT.setError(getString(R.string.error_invalid_password));
+            focusView = passwordT;
+            cancel = true;
+        }
+
+        // Check for a valid email address.
+        if (TextUtils.isEmpty(email)) {
+            emailT.setError(getString(R.string.error_field_required));
+            focusView = emailT;
+            cancel = true;
+        } else if (!isEmailValid(email)) {
+            emailT.setError(getString(R.string.error_invalid_email));
+            focusView = emailT;
+            cancel = true;
+        }
+
+        if (cancel) {
+            // There was an error; don't attempt login and focus the first
+            // form field with an error.
+            focusView.requestFocus();
+        } else {
+            // Show a progress spinner, and kick off a background task to
+            // perform the user login attempt.
+            progressBar.setVisibility(View.VISIBLE);
+            //showProgress(true);
+            //mAuthTask = new UserLoginTask(email, password);
+            //mAuthTask.execute((Void) null);
+
+            //background task to perform register attempt
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d(TAG, "createUserWithEmail:success");
+                                String name = nameT.getText().toString();
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                User newUser = new User(user.getUid(),name,"",user.getEmail(), User.GENDER.MALE,null);
+                                User.addUser(databaseReference.child("users"),storageReference.child("pic"),newUser);
+                                Intent intent = new Intent();
+                                intent.setClass(SignUp.this,MainActivity.class);
+                                startActivity(intent);
+                                //updateUI(user);
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                Toast.makeText(SignUp.this, "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
+                                //updateUI(null);
+                            }
+
+                            // ...
+                        }
+                    });
+
+        }
+
+
+    }
+
+
+    private boolean isEmailValid(String email) {
+        //TODO: Replace this with your own logic
+        return email.contains("@");
+    }
+
+    private boolean isPasswordValid(String password) {
+        //TODO: Replace this with your own logic
+        return password.length() > 4;
+    }
+
+
+
 }
