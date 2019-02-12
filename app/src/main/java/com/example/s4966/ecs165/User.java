729b7c875d2566b8d803ebf3d4ed9eac331b851b
Yuanbo Li <melovche@outlook.com>
2019-02-12 04:27:09 -0800

    add store functionality

--- app/src/main/java/com/example/s4966/ecs165/User.java
+++ app/src/main/java/com/example/s4966/ecs165/User.java
@@ -1,8 +1,20 @@
 package com.example.s4966.ecs165;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.util.Log;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.*;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -14,10 +26,36 @@ public class User {
     private String bio;
     private String email;
     private GENDER gender;
+    private Drawable picture;
+    private String TAG = "User class";
+    /**
+     *  pictureId should always be the same with uid, if exists a picture;
+     */
+    private String pictureId;
+
+    /**
+     * Constructor for the User class.
+     * @param usernameStr username string
+     * @param bioStr bio info string
+     * @param emailStr email string
+     * @param gen gender
+     * @param pic: If there is no picture for the user, please pass null
+     */
+    public User(String usernameStr, String bioStr, String emailStr, GENDER gen, Drawable pic){
+        uid = "";
+        username = usernameStr;
+        bio = bioStr;
+        email = emailStr;
+        gender = gen;
+        picture = pic;
+        pictureId = null;
+    }
+
 
     public String getUsername(){
         return username;
     }
+
 
     public static void addFollow(DatabaseReference databaseFollowsNode, User follow, User followed){
         String TAG = "void addFollow(User follow, User followed)";
@@ -33,24 +71,17 @@ public class User {
         databaseFollowsNode.push().setValue(result);
     }
 
+    // TODO: later we need to delete related picture in database as well
     public static void deleteUser(DatabaseReference databaseUserNode, User user){
         databaseUserNode.child(user.getUid()).removeValue();
     }
 
-    public static void addUser(DatabaseReference databaseUserNode, User user){
+    public static void addUser(DatabaseReference databaseUserNode, StorageReference storagePicNode, User user){
         if (!user.hasUID()) {
             String id = databaseUserNode.push().getKey();
             user.setUid(id);
         }
-        user.toFireBase(databaseUserNode);
-    }
-
-    public User(String id, String usernameStr, String bioStr, String emailStr, GENDER gen){
-        uid = id;
-        username = usernameStr;
-        bio = bioStr;
-        email = emailStr;
-        gender = gen;
+        user.toFireBase(databaseUserNode, storagePicNode);
     }
 
     public boolean hasUID(){
@@ -65,20 +96,41 @@ public class User {
         return uid;
     }
 
-    public User(String usernameStr, String bioStr, String emailStr, GENDER gen){
-        uid = "";
-        username = usernameStr;
-        bio = bioStr;
-        email = emailStr;
-        gender = gen;
-    }
+    private void toFireBase(DatabaseReference databaseUserNode, StorageReference storageImageNode){
+        if(!hasUID()){
+            Log.e(TAG+=" toFireBase", "try to add to firebase when no uid is assigned.");
+            System.exit(1);
+        }
 
-    public void toFireBase(DatabaseReference database){
+        if(picture != null) {
+            pictureId = uid;
+            Bitmap bitmap = ((BitmapDrawable)picture).getBitmap();
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+            byte[] data = baos.toByteArray();
+            UploadTask uploadTask = storageImageNode.child(pictureId).putBytes(data);
+            uploadTask.addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Log.e(TAG += " toFireBase function", "upload picture bytes failure.");
+                    System.exit(1);
+                }
+            }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    return;
+                }
+            });
+        }
         Map<String, Object> result = new HashMap<>();
         result.put("username", username);
         result.put("bio", bio);
         result.put("email", email);
         result.put("gender", gender);
-        database.child(uid).setValue(result);
+        if(pictureId != null){
+            result.put("pictureId", pictureId);
+        }
+        databaseUserNode.child(uid).setValue(result);
     }
 }
+
