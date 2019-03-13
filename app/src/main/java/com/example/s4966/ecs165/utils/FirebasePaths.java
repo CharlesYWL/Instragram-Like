5d27759cb1c9efe9b2db64edd8f6c18d3c19222e
Yuanbo Li <yboli@outlook.com>
2019-03-12 19:52:59 -0700

    add hashtag to database

--- app/src/main/java/com/example/s4966/ecs165/utils/FirebasePaths.java
+++ app/src/main/java/com/example/s4966/ecs165/utils/FirebasePaths.java
@@ -4,4 +4,5 @@ public class FirebasePaths {
     public static String FIREBASE_POSTIMAGE_STORAGE_PATH = "post_pic/users";
     public static String FIREBASE_POST_DATABASE_PATH = "posts";
     public static String FIREBASE_FOLLOWING_PATH = "follows";
+    public static String FIREBASE_HASHTAG_PATH = "hashtags";
 }
